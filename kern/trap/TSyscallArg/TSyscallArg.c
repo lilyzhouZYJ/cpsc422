a0b24227be59d3a7d44c5ed7836080953bdc6924
Lily Zhou <lzhouzyj@gmail.com>
2022-10-11 00:04:33 -0400

    Finish TSyscallArg.c (10/11)

--- kern/trap/TSyscallArg/TSyscallArg.c
+++ kern/trap/TSyscallArg/TSyscallArg.c
@@ -12,37 +12,43 @@ extern tf_t uctx_pool[NUM_IDS];
 unsigned int syscall_get_arg1(void)
 {
     // TODO
-    return 0;
+	int curr_pid = get_curid();
+    return uctx_pool[curr_pid].regs.eax; // system call number in %eax
 }
 
 unsigned int syscall_get_arg2(void)
 {
     // TODO
-    return 0;
+    int curr_pid = get_curid();
+    return uctx_pool[curr_pid].regs.ebx; // arg1: ebx
 }
 
 unsigned int syscall_get_arg3(void)
 {
     // TODO
-    return 0;
+    int curr_pid = get_curid();
+    return uctx_pool[curr_pid].regs.ecx; // arg2: ecx
 }
 
 unsigned int syscall_get_arg4(void)
 {
     // TODO
-    return 0;
+    int curr_pid = get_curid();
+    return uctx_pool[curr_pid].regs.edx; // arg3: edx
 }
 
 unsigned int syscall_get_arg5(void)
 {
     // TODO
-    return 0;
+    int curr_pid = get_curid();
+    return uctx_pool[curr_pid].regs.esi; // arg4: esi
 }
 
 unsigned int syscall_get_arg6(void)
 {
     // TODO
-    return 0;
+    int curr_pid = get_curid();
+    return uctx_pool[curr_pid].regs.edi; // arg5: edi
 }
 
 /**
@@ -52,6 +58,8 @@ unsigned int syscall_get_arg6(void)
 void syscall_set_errno(unsigned int errno)
 {
     // TODO
+	int curr_pid = get_curid();
+	uctx_pool[curr_pid].err = errno;
 }
 
 /**
@@ -61,24 +69,34 @@ void syscall_set_errno(unsigned int errno)
 void syscall_set_retval1(unsigned int retval)
 {
     // TODO
+	int curr_pid = get_curid();
+    uctx_pool[curr_pid].regs.ebx = retval; // arg1: ebx
 }
 
 void syscall_set_retval2(unsigned int retval)
 {
     // TODO
+	int curr_pid = get_curid();
+    uctx_pool[curr_pid].regs.ecx = retval; // arg2: ecx
 }
 
 void syscall_set_retval3(unsigned int retval)
 {
     // TODO
+	int curr_pid = get_curid();
+    uctx_pool[curr_pid].regs.edx = retval; // arg3: edx
 }
 
 void syscall_set_retval4(unsigned int retval)
 {
     // TODO
+	int curr_pid = get_curid();
+    uctx_pool[curr_pid].regs.esi = retval; // arg4: esi
 }
 
 void syscall_set_retval5(unsigned int retval)
 {
     // TODO
+	int curr_pid = get_curid();
+    uctx_pool[curr_pid].regs.edi = retval; // arg5: edi
 }
