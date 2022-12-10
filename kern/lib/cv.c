1800d18800b98c6733dc5bffa864fd43f71e0260
Lily Zhou <lzhouzyj@gmail.com>
2022-12-10 13:04:38 -0500

    Change cv.c and cv.h to lab4 solution code

--- kern/lib/cv.c
+++ kern/lib/cv.c
@@ -1,35 +1,134 @@
-#include <lib/spinlock.h>
+#include <lib/cv.h>
+#include <lib/debug.h>
+#include <lib/string.h>
 #include <lib/thread.h>
 #include <dev/intr.h>
+#include <thread/PCurID/export.h>
 #include <thread/PThread/export.h>
-#include "cv.h"
 
-void CV_init(struct CV *cv)
+void CV_init(CV *cv)
 {
-    cv_queue_init(&(cv->queue));
-}
-
-void CV_wait(struct CV *cv, spinlock_t *lk)
-{
-    // (1) Add TCB of current thread to waiting queue
-    unsigned int curid = get_curid();
-	cv_queue_enqueue(&(cv->queue), curid);
-
-    // (2) Release BBQueue lock: this lock protects CV + BBQueue
-    spinlock_release(lk);
-
-	// (3) Switch to new thread
-    thread_suspend();
-
-	// (4) Re-acquire lock
-	spinlock_acquire(lk);
-}
-
-void CV_signal(struct CV *cv) {
-    // Check if waiting queue is empty
-    if(cv_queue_is_empty(&(cv->queue)) == 0){
-        // Not empty: add new thread TCB to ready list of its original CPU
-        unsigned int new_thread_id = cv_queue_dequeue(&(cv->queue));
-        thread_resume(new_thread_id);
+    int i;
+    cv->tail = 0;
+    for (i = 0; i < NUM_IDS; i++) {
+        cv->queue[i] = 0;
     }
+}
+
+static void CV_enqueue(CV *cv, unsigned int pid)
+{
+    NO_INTR(
+        KERN_ASSERT(0 < pid && pid < NUM_IDS);
+        KERN_ASSERT(0 <= cv->tail && cv->tail < NUM_IDS);
+        KERN_ASSERT(cv->queue[cv->tail] == 0);
+        KERN_ASSERT(cv->queue[pid] == 0)
+    );
+
+    cv->queue[cv->tail] = pid;
+    cv->tail = pid;
+}
+
+static unsigned int CV_dequeue(CV *cv)
+{
+    unsigned int pid = cv->queue[0];
+
+    if (cv->queue[pid] == 0) {
+        cv->tail = 0;
+    }
+    cv->queue[0] = cv->queue[pid];
+    cv->queue[pid] = 0;
+
+    return pid;
+}
+
+void CV_wait(CV *cv, spinlock_t *lk)
+{
+    unsigned int old_cur_pid;
+    NO_INTR(
+        KERN_ASSERT(spinlock_holding(lk))
+    );
+
+    old_cur_pid = get_curid();
+    CV_enqueue(cv, old_cur_pid);
+
+    NO_INTR(
+        thread_suspend(lk, old_cur_pid)
+    );
+
+    spinlock_acquire(lk);
+}
+
+void CV_signal(CV *cv)
+{
+    unsigned int pid = CV_dequeue(cv);
+    if (pid != 0) {
+        NO_INTR(
+            thread_ready(pid)
+        );
+    }
+}
+
+void CV_broadcast(CV *cv)
+{
+    unsigned int pid;
+
+    while ((pid = CV_dequeue(cv)) != 0) {
+        NO_INTR(
+            thread_ready(pid)
+        );
+    }
+}
+
+void BB_init(BoundedBuffer *bb)
+{
+    memzero(bb->buf, BUFFER_SIZE);
+    bb->head = bb->size = 0;
+    spinlock_init(&bb->lk);
+    CV_init(&bb->empty);
+    CV_init(&bb->full);
+}
+
+bool BB_is_empty(BoundedBuffer *bb)
+{
+    return bb->size == 0;
+}
+
+bool BB_is_full(BoundedBuffer *bb)
+{
+    return bb->size == BUFFER_SIZE;
+}
+
+void BB_enqueue(BoundedBuffer *bb, unsigned int val)
+{
+    unsigned int idx;
+    spinlock_acquire(&bb->lk);
+
+    while (BB_is_full(bb)) {
+        CV_wait(&bb->full, &bb->lk);
+    }
+
+    idx = (bb->head + bb->size) % BUFFER_SIZE;
+    bb->buf[idx] = val;
+    bb->size++;
+
+    CV_signal(&bb->empty);
+    spinlock_release(&bb->lk);
+}
+
+unsigned int BB_dequeue(BoundedBuffer *bb)
+{
+    unsigned int val;
+    spinlock_acquire(&bb->lk);
+
+    while (BB_is_empty(bb)) {
+        CV_wait(&bb->empty, &bb->lk);
+    }
+
+    val = bb->buf[bb->head];
+    bb->head = (bb->head + 1) % BUFFER_SIZE;
+    bb->size--;
+
+    CV_signal(&bb->full);
+    spinlock_release(&bb->lk);
+    return val;
 }
--- kern/lib/cv.h
+++ kern/lib/cv.h
@@ -1,26 +1,37 @@
-#ifndef _KERN_LIB_KSTACK_H_
-#define _KERN_LIB_KSTACK_H_
+#ifndef _KERN_LIB_CV_H_
+#define _KERN_LIB_CV_H_
 
 #ifdef _KERN_
 
-#include <dev/intr.h>
-#include <thread/PThread/export.h>
+#include <lib/spinlock.h>
+#include <lib/x86.h>
 
-struct TQueue {
-    unsigned int head;
+typedef struct {
     unsigned int tail;
-};
+    unsigned int queue[NUM_IDS];
+} CV;
 
-struct CV {
-    struct TQueue queue;
-};
+typedef struct {
+#define BUFFER_SIZE 3
+    unsigned int buf[BUFFER_SIZE];
+    unsigned int head;
+    unsigned int size;
+    spinlock_t lk;
+    CV empty;
+    CV full;
+} BoundedBuffer;
 
-void CV_init(struct CV *cv);
+void CV_init(CV *cv);
+void CV_wait(CV *cv, spinlock_t *lk);
+void CV_signal(CV *cv);
+void CV_broadcast(CV *cv);
 
-void CV_wait(struct CV *cv, spinlock_t *lk);
-
-void CV_signal(struct CV *cv);
+void BB_init(BoundedBuffer *bb);
+bool BB_is_empty(BoundedBuffer *bb);
+bool BB_is_full(BoundedBuffer *bb);
+void BB_enqueue(BoundedBuffer *bb, unsigned int val);
+unsigned int BB_dequeue(BoundedBuffer *bb);
 
 #endif  /* _KERN_ */
 
-#endif  /* !_KERN_LIB_KSTACK_H_ */
+#endif  /* !_KERN_LIB_CV_H_ */
